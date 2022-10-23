b88db7d11b5844072d9ee588e2828d4f77806ec9
devihall <devis0306@gmail.com>
2022-10-23 19:19:01 -0400

    projects component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
+import Projects from "./components/Projects/Projects"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Projects.css'
+
+const Projects = ()=>{
+    return(
+        <div className="projects">
+            <div className="awesome">
+                <span>My awesome</span>
+                <span>projects.</span>
+            </div>
+            <div className="blur projects-blur"></div>
+            <div className="cards">
+                Iam the right side
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
