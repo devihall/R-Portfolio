6d1fa54873f228ee14aaafc51b27ef887a5ba4aa
devihall <devis0306@gmail.com>
2022-10-23 21:12:03 -0400

    styling for card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Card.css'
+
+const Card = ({firstProject, title, technologies})=>{
+    return(
+        <div className="card">
+            <img src={firstProject} alt="project"/>
+            <span>{title}</span>
+            <span>{technologies}</span>
+            <button className="card-button">Learn More</button>
+        </div>
+    )
+}
+
+
+export default Card;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,28 @@
 import React from "react";
-import './Projects.css'
+import "./Projects.css";
+import Card from "../Card/Card.js";
+import ProjectOne from "../../img/project-1.png";
 
-const Projects = ()=>{
-    return(
-        <div className="projects">
-            <div className="awesome">
-                <span>My awesome</span>
-                <span>projects.</span>
-            </div>
-            <div className="blur projects-blur"></div>
-            <div className="cards">
-                Iam the right side
-            </div>
-        </div>
-    )
-}
+const Projects = () => {
+  return (
+    <div className="projects">
+      <div className="awesome">
+        <span>My awesome</span>
+        <span>projects.</span>
+      </div>
+      <div
+        className="blur projects-blur1"
+        style={{ background: "#ABF1FF94" }}
+      ></div>
+      <div className="cards">
+        <Card
+          firstProject={ProjectOne}
+          title={"ZooKeepr"}
+          technologies={ "Node, Express, JavaScript"}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
