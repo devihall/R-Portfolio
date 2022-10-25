09125c2800ee90fd74a3360e1e5f0ffd063c7ccc
devihall <devis0306@gmail.com>
2022-10-25 16:10:17 -0400

    conditional rendering of contact form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
-// import About from './components/AboutMe/AboutMe'
-import Projects from "./components/Projects/Projects";
-// import Contact from "./components/ContactMe/ContactMe"
+// import About from "./components/About/About";
+// import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  
+  
+
   return (
     <div className="App">
-      <Navbar />
-      <Intro />
-      <Projects />
+      <Navbar
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      > </Navbar>
+
+      <main>
+        {!contactSelected ? (
+          <>
+            <Intro />
+          </>
+        ) : (
+          <Contact></Contact>
+        )}
+      </main>
     </div>
   );
 }
-
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,31 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const {
+    contactSelected,
+    setContactSelected
+  } = props;
   return (
     <div className="nav-wrapper">
       <div className="nav-left">
-        <div className="nav-name">Dévi Hall</div>
+        <div className="nav-name"><a data-testid="link" href="/">Dévi Hall</a></div>
         <span>Toggle</span>
       </div>
       <div className="nav-right">
         <div className="nav-list">
           <ul>
             <li>About Me</li>
-            <li>Portfolio</li>
+            <li>Projects</li>
             <li>Resume</li>
           </ul>
         </div>
       </div>
-      <button className="button nav-button">Contact Me</button>
+      <button
+        className="button nav-button"
+        onClick={() => setContactSelected(true)}>
+        Contact Me
+      </button>
     </div>
   );
 };
