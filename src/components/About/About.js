5e70b67ef1f2db35fb96c3ccfb221b03c8ea9de5
devihall <devis0306@gmail.com>
2022-10-26 10:16:22 -0400

    about me text added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,42 @@ import React from "react";
 
 const About = () => {
   return (
-  <h1>About Me</h1>
-  )
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am a career changer, switching from the fields of Science education
+        and research to scientific computing. My first formal software
+        development project was for my graduate thesis where, using front end
+        tools, I built an user friendly repository for species based scientific
+        literature reviews; 
+        </p>
+
+        <p>I am an experienced full stack web/software
+        developer, currently completing a course in Full Stack Web Development
+        at University of Miami. During my course I had the opportunity to work
+        on a collaborative group project, where we conceived and executed a CMS
+        application that solved a real-world problem. This full-stack
+        application, combined a robust back end—with servers, databases,
+        advanced APIs, and user authentication. I have experience in creating my
+        own server-side APIs using the Express.js framework. In addition to
+        building the Express.js API, I have had practice connecting the back end
+        to a relational database using MySQL. Additionally, I have experience
+        taking a working Express.js API and configuring it to use Sequelize to
+        interact with a MySQL database. I have developed several applications
+        following the RESTful API architectural pattern. In addition to
+        applications that I developed to run in the browser and feature
+        dynamically updated HTML and CSS, fetching data from third party API's.
+      </p>
+
+      <p>
+        I have a unique combination of skills and work experience in the field
+        of Science education and research. My tenure in education and research
+        allowed me to sharpen my soft skills in communication, critical
+        thinking, time management and empathy, and I have the ability to work
+        independently to achieve goals in a fast paced, agile environment.
+      </p>
+    </div>
+  );
 };
 
 export default About;
