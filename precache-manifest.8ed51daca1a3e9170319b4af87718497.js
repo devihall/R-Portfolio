01f23990a145012ae44dba1b4f265467ea061e6c
devihall <devis0306@gmail.com>
2022-10-27 16:33:22 -0400

    Updates

---
+++ precache-manifest.8ed51daca1a3e9170319b4af87718497.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7d499babf0a797ca9ca",
+    "url": "/R-Portfolio/static/css/main.50f47bab.chunk.css"
+  },
+  {
+    "revision": "b7d499babf0a797ca9ca",
+    "url": "/R-Portfolio/static/js/main.b7d499ba.chunk.js"
+  },
+  {
+    "revision": "ce8c896f80c757d5966b",
+    "url": "/R-Portfolio/static/js/1.ce8c896f.chunk.js"
+  },
+  {
+    "revision": "d146c576070ac28f1ffa",
+    "url": "/R-Portfolio/static/js/runtime~main.d146c576.js"
+  },
+  {
+    "revision": "6aa4396362357ad50903091c60aec68a",
+    "url": "/R-Portfolio/static/media/DHallRESUME2022 .6aa43963.pdf"
+  },
+  {
+    "revision": "50715842690a7233811b3967d6cb40e7",
+    "url": "/R-Portfolio/static/media/intro-pic.50715842.png"
+  },
+  {
+    "revision": "41085ed60cc9f83bafb2c5a44e9841c9",
+    "url": "/R-Portfolio/static/media/about-img.41085ed6.png"
+  },
+  {
+    "revision": "3a96f40c74556095894ff895d7f044a4",
+    "url": "/R-Portfolio/static/media/project-1.3a96f40c.png"
+  },
+  {
+    "revision": "6842453aecb2ab27d50e039f6efa357f",
+    "url": "/R-Portfolio/static/media/project-2.6842453a.png"
+  },
+  {
+    "revision": "316b559958e58d50adfc97695ab06385",
+    "url": "/R-Portfolio/static/media/project-3.316b5599.png"
+  },
+  {
+    "revision": "787cbff6a41bbf031250238b451d5c0d",
+    "url": "/R-Portfolio/static/media/project-4.787cbff6.png"
+  },
+  {
+    "revision": "fbb2bc661815eea990c208a11cdd6922",
+    "url": "/R-Portfolio/index.html"
+  }
+];
